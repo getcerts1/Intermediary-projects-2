184b35f769e59418f134f110a05659f433e9cdf0
getcerts1 <barrowtech1999@gmail.com>
2024-12-25 15:36:32 +0300

    added new functionality with the random choice and the display on the GUI

--- randomwalk.py
+++ randomwalk.py
@@ -1,35 +1,43 @@
-import turtle
 import turtle as t
 import random
 
 
 timmy = t.Turtle()
-#screen = t.Screen()
+screen = t.Screen()
+t.colormode(255)
 
-static_colors = [
-    "black", "red", "blue", "green", "purple",
-    "orange", "dark cyan", "magenta", "brown",
-    "dark slate gray", "cyan", "yellow", "gold"
-]
-dynamic_colors = [
-
-]
-
+colors_list = []
 
 def rgb_color():
     r = random.randint(0,255)
     g = random.randint(0,255)
     b = random.randint(0,255)
 
-    output = f'({r},{g},{b})'
+    output = (r,g,b)
     return output
 
-def color_list_gen(number_of_colors):
-    for i in range(number_of_colors):
-        random_color = (rgb_color())
-        colors.append(random_color)
+def color_list_gen(number_of_colors, lists):
 
-    for i, color in enumerate(colors):
-        if colors.index(color) == 0:
+    for i in range(number_of_colors + 1):
+        random_color = (rgb_color())
+        lists.append(random_color)
+
+    for i, color in enumerate(lists):
+        if lists.index(color) == 0:
             continue
-        print(f'color {i}: {color}')
+        print(f"{i}:{color}")
+
+
+color_list_gen(20, colors_list)
+
+directions = [0, 90,180,270]
+timmy.pensize(20)
+timmy.speed("fastest")
+
+for i in range(100):
+    timmy.pencolor(random.choice(colors_list))
+    timmy.setheading(random.choice(directions))
+    timmy.forward(40)
+
+
+screen.exitonclick()
