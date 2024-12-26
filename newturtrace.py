a40e1ec517c1957c22d79a852935c283319fc063
getcerts1 <barrowtech1999@gmail.com>
2024-12-26 18:22:16 +0300

    file runs another file, trying to fix

---
+++ newturtrace.py
@@ -0,0 +1,22 @@
+import turtle as t
+import random
+
+from hirst import sammy, screen
+
+tolcan = t.Turtle()
+tommy = t.Turtle()
+Sammy = t.Turtle()
+
+tolcan.shape("turtle")
+tommy.shape("turtle")
+sammy.shape("turtle")
+
+for i in range(10):
+    tolcan.forward(random.randint(1,10))
+
+
+
+
+
+
+screen.exitonclick()
