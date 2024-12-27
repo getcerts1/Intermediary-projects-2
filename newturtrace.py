d22c67b2975c0b7f2a959ad93eda1a1f7cff8307
getcerts1 <barrowtech1999@gmail.com>
2024-12-27 23:10:02 +0300

    build most of the functionality for the turtle game, next I will try and call an API that returns the color names for RGB

--- newturtrace.py
+++ newturtrace.py
@@ -1,22 +1,73 @@
 import turtle as t
 import random
 
-from hirst import sammy, screen
 
-tolcan = t.Turtle()
-tommy = t.Turtle()
-Sammy = t.Turtle()
 
-tolcan.shape("turtle")
-tommy.shape("turtle")
-sammy.shape("turtle")
+screen = t.Screen()
+screen.setup(500,400)
+screen.colormode(255)
 
-for i in range(10):
-    tolcan.forward(random.randint(1,10))
+#let user add max 7 competitors to the game
+names = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace"]
+random_colors = []
+turtles_objects = {}
 
 
 
+while True:
+    try:
+        Players = int(screen.numinput("Players", "Enter the number of competitors"))
+        if Players <= 0 or Players > 8:
+            raise ValueError("The number you entered is outside the scope. Please enter a value between 1 and 8.")
+        else:
+            break
+    except ValueError as e:
+        print(e)
 
 
+for i in range(Players):
+    r = random.randint(50,200)
+    g = random.randint(50, 200)
+    b = random.randint(50,200)
+    random_colors.append((r,g,b))
+
+for i in range(Players):
+    random_name = random.choice(names)
+    turtles_objects[random_name] = t.Turtle()
+    names.remove(random_name)
+
+
+x_axis = -240
+y_axis = 180
+for key,value in turtles_objects.items():
+    print(f"{key}:{value}")
+    turtles_objects[key].shape("turtle")
+    random_color = random.choice(random_colors)
+    turtles_objects[key].color(random_color)
+    turtles_objects[key].goto(x_axis, y_axis)
+    y_axis -= 400 / Players
+
+
+try:
+    Bet = screen.textinput("Bets", "Who will win? Enter a color")
+    if Bet is None or Bet not in random_colors:
+        raise ValueError("Invalid bet")
+except ValueError as e:
+    print(f"Error: {e}")
+    Bet = None  # Handle the case by assigning a default or exiting
+
+
+    while True:
+        for key in turtles_objects.keys():
+            turtles_objects[key].forward(random.randint(5,50))
+            x, y = turtles_objects[key].position()
+            print(f'{[key]} -- {turtles_objects[key].position()}')
+            if x >= 250.00:
+                print(f'The winner is {key}')
+                print(f'The winner was {key.color}')
+                break
+        else:
+            continue
+        break
 
 screen.exitonclick()
---
+++ test.py
@@ -0,0 +1,3 @@
+tup = (918,2)
+second_element = tup[1]
+print(second_element)
